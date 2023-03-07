91441758e81aa0abb03dbded034742c6b18b923a
WaRtrO <morotmaxence@live.fr>
2023-03-07 23:08:32 +0100

    Add files via upload

---
+++ tools/install/php/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IFS=' ' read -r -a array <<< "$@"
+
+echo "You install the PHP version : "${array[0]}
+apt update -y
+apt install libapache2-mod-php${array[0]} php${array[0]} -y
+apt install php${array[0]}-{curl,gd,intl,memcache,xml,zip,mbstring} -y
+apt install php${array[0]}-fpm php${array[0]}-mysql -y
+cd ../
