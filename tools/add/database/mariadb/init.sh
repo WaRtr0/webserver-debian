bfa47dd0d74084c279278ed0592c85b543ee7d88
WaRtrO <morotmaxence@live.fr>
2023-03-09 02:06:13 +0100

    Update init.sh

--- tools/add/database/mariadb/init.sh
+++ tools/add/database/mariadb/init.sh
@@ -5,5 +5,5 @@ IFS=' ' read -r -a array <<< "$@"
 mysql << EOF
 CREATE USER IF NOT EXISTS '${array[0]}'@'localhost' IDENTIFIED BY '${array[1]}';
 CREATE DATABASE ${array[0]}_${array[2]};
-GRANT ALL PRIVILEGES ON `${array[0]}_${array[2]}`.* TO `${array[0]}`@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `${array[0]}_${array[2]}`.* TO '${array[0]}'@'localhost' WITH GRANT OPTION;
 EOF
