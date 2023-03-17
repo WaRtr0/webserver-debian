ce1a2df9f950c31b9b41763a1e2c8c0f248980ec
WaRtrO <morotmaxence@live.fr>
2023-03-17 04:18:50 +0100

    Update init.sh

--- tools/install/php/init.sh
+++ tools/install/php/init.sh
@@ -5,6 +5,6 @@ IFS=' ' read -r -a array <<< "$@"
 echo "You install the PHP version : "${array[0]}
 apt update -y
 apt install libapache2-mod-php${array[0]} php${array[0]} -y
-apt install php${array[0]}-{curl,gd,intl,memcache,xml,zip,mbstring} -y
+apt install php${array[0]}-{curl,gd,intl,memcache,xml,zip,mbstring,redis} -y
 apt install php${array[0]}-fpm php${array[0]}-mysql -y
 cd ../
